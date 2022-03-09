aceae4409fba45b1f917241ef0f2def209a249e5
Samarth Patel <psamarth8836@gmail.com>
2022-03-08 22:22:07 -0500

    changed a few styles

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -47,14 +47,14 @@ const Footer = () => {
       <div className="app__footer-cards">
         <div className="app__footer-card ">
           <img src={images.email} alt="email" />
-          <a href="mailto:hello@micael.com" className="p-text">
-            hello@micael.com
+          <a href="mailto:psamarth8836@gmail.com" className="p-text">
+            psamarth8836@gmail.com
           </a>
         </div>
         <div className="app__footer-card">
           <img src={images.mobile} alt="phone" />
-          <a href="tel:+1 (123) 456-7890" className="p-text">
-            +1 (123) 456-7890
+          <a href="tel:+1 (648) 879-3836" className="p-text">
+            +1 (648) 879-3836
           </a>
         </div>
       </div>
